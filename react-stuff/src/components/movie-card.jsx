be159ec07922f142c7da7de500fdf433bcad52ef
Jnkemokorie <160935632+Jnkemokorie@users.noreply.github.com>
2026-03-11 15:41:22 +0000

    a bit of css

--- react-stuff/src/components/movie-card.jsx
+++ react-stuff/src/components/movie-card.jsx
@@ -1,8 +1,8 @@
-
+import styles from "./moviecard.module.css"
 
 const MovieCard = ({ title, director, year, rating }) => {
     return (
-        <div>
+        <div className={styles.center}>
             <h1>{title}</h1>
             <p>{director}</p>
             <p>{year}</p>
